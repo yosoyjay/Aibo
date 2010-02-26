944e9591322c2728151549cf5f088265dbcf48df
Joel Gonzalez <datasieve@gmail.com>
2010-02-25 21:19:25 -0500

    Added Aibo: Head and Walk. Also cleaned up / formalized the rest of the codebase

---
+++ src/AiboHead.h
@@ -0,0 +1,26 @@
+#ifndef AIBO_HEAD_H
+#define AIBO_HEAD_H
+
+#include "AiboNet.h"
+
+const int AIBO_HEAD_PORT = 10052;
+
+class AiboHead
+{
+public:
+    AiboHead();
+    void connect(char ip_addr[]);
+    int up(float magnitude);
+    int down(float magnitude);
+    int left(float magnitude);
+    int right(float magnitude);
+    int pitch(float magnitude);
+    int yaw(float magnitude);
+    ~AiboHead();
+
+private:
+    AiboNet *aibolink;
+
+};
+
+#endif
---
+++ src/AiboWalk.cc
@@ -0,0 +1,68 @@
+#include "AiboWalk.h"
+#include "unistd.h"
+
+AiboWalk::AiboWalk()
+{
+
+}
+
+void AiboWalk::connect(char ip_addr[]){
+
+  aibolink = new AiboNet(ip_addr, AIBO_WALK_PORT);
+
+}
+
+int AiboWalk::forward(float magnitude)
+{
+
+    aibolink->send_data('f', magnitude);
+    usleep(500000);
+    return 0;
+}
+
+int AiboWalk::backward(float magnitude)
+{
+
+    aibolink->send_data('f', -magnitude);
+    usleep(500000);
+    return 0;
+}
+
+int AiboWalk::strafe_left(float magnitude)
+{
+
+    aibolink->send_data('s', magnitude);
+    usleep(500000);
+    return 0;
+}
+
+int AiboWalk::strafe_right(float magnitude)
+{
+
+    aibolink->send_data('s', -magnitude);
+    usleep(500000);
+    return 0;
+}
+
+int AiboWalk::rotate_clockwise(float magnitude)
+{
+
+    aibolink->send_data('r', magnitude);
+    usleep(500000);
+    return 0;
+}
+
+int AiboWalk::rotate_counter_clockwise(float magnitude)
+{
+
+    aibolink->send_data('r', -magnitude);
+    usleep(500000);
+    return 0;
+}
+
+AiboWalk::~AiboWalk()
+{
+
+  delete aibolink;
+
+}
